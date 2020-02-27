0b56fd045cedf9e2e7b9fe4b5c1bb6ee7fbd7d31
spiridonovai <i.spiridonova@yahoo.com>
2020-02-27 15:45:55 +0100

    irena

--- controllers/uam.controller.js
+++ controllers/uam.controller.js
@@ -1,101 +1,126 @@
-const AWS = require('aws-sdk');
-var cognito = require('amazon-cognito-identity-js-node');
-global.fetch = require('node-fetch');
-var poolData = { UserPoolId : 'eu-central-1_lnIYhI1GD',
-    ClientId : '1mhv695m2in34eqblp8hb2p4i5'
+const AWS = require("aws-sdk");
+var cognito = require("amazon-cognito-identity-js-node");
+global.fetch = require("node-fetch");
+var poolData = {
+  UserPoolId: "eu-central-1_rw3G5TW7b",
+  ClientId: "gukhocq1nfqkdvuimfojf6nbb"
 };
 var userPool = new cognito.CognitoUserPool(poolData);
+var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 const UAMController = {
-    signup: function (req, res) {
-        var method = req.method;
-        if(method !== 'GET') {
-            res.status(403);
-            res.send('Forbidden')
-        }
-        res.sendFile('signup.html', {root: 'views'});
-    },
-    register: function (req, res) {
-        var method = req.method;
-        console.log(req);
-        var params = req.body;
-        if(method == 'GET') {
-            res.status(403);
-            res.send('Forbidden')
-        }
-
-        res.status(200);
-        var attributeEmail = new cognito.CognitoUserAttribute('email', params.email);
-        userPool.signUp(params.email, params.password, [attributeEmail], null, function(err, result) {
-                if (err) {
-                    console.log("Error:");
-                    console.log(err);
-                    return;
-                }
-            }
-        )
-        res.redirect('/');
-    },
-    login: function (req, res) {
-        var method = req.method;
-        if(method !== 'GET') {
-            res.status(403);
-            res.send('Forbidden')
-        }
-        res.sendFile('login.html', {root: 'views'});
-    },
-    authenticate: function(req, res) {
-        var method = req.method;
-        var params = req.body;
-        console.log(req);
-        if(method == 'GET') {
-            res.status(403);
-            res.send('Forbidden')
-        }
-        var authData ={
-            Username: params.email,
-            Password: params.password
-        };
-        var authenticationDetails = new cognito
-            .AuthenticationDetails(authData);
-        //pool data goes here from outside
-        //user pool goes here from outside
-        var userData = {
-            Username : params.email,
-            Pool : userPool
-        };
-        var cognitoUser = new cognito.CognitoUser(userData);
-
-
-        cognitoUser.authenticateUser(authenticationDetails, {
-            onSuccess: function (result) {
-                var accessToken = result.getAccessToken().getJwtToken();
-                res.status(200);
-                res.sendFile('myaccount.html', {root: 'views'});
-            },
-            onFailure: function(err) {
-                res.redirect('/user/login')
-            }});
-    },
-    logout: function (req, res) {
-        console.log()
-    },
-    myaccount: function (req, res) {
-        var method = req.method;
-        if(method !== 'GET') {
-            res.status(403);
-            res.send('Forbidden')
-        }
-        res.sendFile('myaccount.html', {root: 'views'});
-    },
-    mywallet: function (req, res) {
-        var method = req.method;
-        if(method !== 'get') {
-            res.status(403);
-            res.send('Forbidden')
-        }
-        res.sendFile('mywallet.html', {root: 'views'});
+  signup: function(req, res) {
+    var method = req.method;
+    if (method !== "GET") {
+      res.status(403);
+      res.send("Forbidden");
     }
-}
+    res.sendFile("signup.html", { root: "views" });
+  },
+  register: function(req, res) {
+    var method = req.method;
+    var params = req.body;
+    if (method == "GET") {
+      res.status(403);
+      res.send("Forbidden");
+    }
+
+    res.status(200);
+    var attributeEmail = new cognito.CognitoUserAttribute(
+      "email",
+      params.email
+    );
+    userPool.signUp(
+      params.email,
+      params.password,
+      [attributeEmail],
+      null,
+      function(err, result) {
+        if (err) {
+          console.log("Error:");
+          console.log(err);
+          return;
+        }
+      }
+    );
+    // let params = {
+    //     UserPoolId: poolData.UserPoolId, /* required */
+    //     Username: 'STRING_VALUE', /* required */
+    //     ClientMetadata: {
+    //       '<StringType>': 'STRING_VALUE',
+    //       /* '<StringType>': ... */
+    //     }
+    //   };
+    // cognitoidentityserviceprovider.confirmSignUp(params, function(err, data) {
+    //     if (err) console.log(err, err.stack); // an error occurred
+    //     else     console.log(data);           // successful response
+    //   });
+    res.redirect("/");
+  },
+  login: function(req, res) {
+    var method = req.method;
+    if (method !== "GET") {
+      res.status(403);
+      res.send("Forbidden");
+    }
+    res.sendFile("login.html", { root: "views" });
+  },
+  authenticate: function(req, res) {
+    var method = req.method;
+    var params = req.body;
+
+    if (method == "GET") {
+      res.status(403);
+      res.send("Forbidden");
+    }
+    var authData = {
+      Username: params.email,
+      Password: params.password
+    };
+    var authenticationDetails = new cognito.AuthenticationDetails(authData);
+    //pool data goes here from outside
+    //user pool goes here from outside
+    var userData = {
+      Username: params.email,
+      Pool: userPool
+    };
+    var cognitoUser = new cognito.CognitoUser(userData);
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function(result) {
+        var id_token = result.getIdToken();
+        var refresh_token = result.getRefreshToken();
+        res.status(200);
+        res.setHeader("Authorization", id_token.getJwtToken());
+        res.setHeader("refresh_token", refresh_token.getToken());
+        res.redirect("/user/myaccount");
+      },
+      onFailure: function(err) {
+        console.log(err);
+        res.redirect("/user/login");
+      }
+    });
+  },
+  logout: function(req, res) {
+    console.log();
+  },
+  external: function(req, res) {},
+  myaccount: function(req, res) {
+    var method = req.method;
+    if (method !== "GET") {
+      res.status(403);
+      res.send("Forbidden");
+    }
+    res.sendFile("myaccount.html", { root: "views" });
+  },
+  mywallet: function(req, res) {
+    var method = req.method;
+    if (method !== "get") {
+      res.status(403);
+      res.send("Forbidden");
+    }
+    res.sendFile("mywallet.html", { root: "views" });
+  }
+};
 
 module.exports = UAMController;
--- router.js
+++ router.js
@@ -1,31 +1,79 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios").default;
 const router = express.Router();
-const HomeController = require('./controllers/home.controller');
-const UAMController = require('./controllers/uam.controller');
+const HomeController = require("./controllers/home.controller");
+const UAMController = require("./controllers/uam.controller");
+const qs = require("qs");
 
-router.get('/user/:method', function (req, res) {
-    var method = req.params.method;
-    if(typeof UAMController[method] == 'function')
-        UAMController[method].call(this, req, res);
-    else {
-        res.redirect('/404');
-    }
+router.get("/user/:method", function(req, res) {
+  var method = req.params.method;
+  if (typeof UAMController[method] == "function")
+    UAMController[method].call(this, req, res);
+  else {
+    res.redirect("/404");
+  }
 });
-router.post('/user/:method', function (req, res) {
-    var method = req.params.method;
-    console.log("Params:", req);
-    if(typeof UAMController[method] === 'function')
-        UAMController[method](req, res);
-    else {
-        res.redirect('/404');
-    }
+router.post("/user/:method", function(req, res) {
+  var method = req.params.method;
+  console.log("Params:", req);
+  if (typeof UAMController[method] === "function")
+    UAMController[method](req, res);
+  else {
+    res.redirect("/404");
+  }
 });
-router.get('/', function (req, res) {
-    HomeController.serveHome(req, res);
+router.get("/", function(req, res) {
+  HomeController.serveHome(req, res);
+});
+router.get("/post-authentication", async function(req, res) {
+  var code = req.query.code;
+  console.log(code);
+  try {
+      axios.defaults.headers.post['Content-Type']='application/x-www-form-urlencoded';
+      axios.defaults.headers.post['Accepts']='*/*';
+   return axios.post("https://pouch-test.auth.eu-central-1.amazoncognito.com/oauth2/token", {
+    grant_type: "authorization_code",
+    code: code,
+    client_id: "gukhocq1nfqkdvuimfojf6nbb",
+    redirect_uri: "http://localhost:3000/post-authentication"
+   }).then(data => {
+       console.log(data);
+       return data;
+   })
+  } catch (e) {
+    console.log(e.response);
+    res.sendStatus(200);
+  }
+  res.sendStatus(200);
 });
 
-router.get('/404', function (req, res) {
-   res.send('ERROR 404');
+router.get("/404", function(req, res) {
+  res.send("ERROR 404");
 });
+
+async function getToken(code) {
+  try {
+    let data = {
+      grant_type: "authorization_code",
+      code: code,
+      client_id: "gukhocq1nfqkdvuimfojf6nbb",
+      redirect_uri: "http://localhost:3000/post-authentication"
+    };
+    console.log(qs.stringify(data));
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+        Accept: "*/*"
+      },
+      data: qs.stringify(data),
+      url: "https://pouch-test.auth.eu-central-1.amazoncognito.com/oauth2/token"
+    };
+    return axios(options);
+  } catch (e) {
+    console.log(e.response);
+    return {};
+  }
+}
 
 module.exports = router;
